57af67a1e5e078609265ede4bf24c7db54603674
cch137 <96408836+cch137@users.noreply.github.com>
2026-01-09 21:59:11 +0800

    refactor(object-designer): replace v1 with v2
    - object designer 正式以 v2 取代 v1。
    - 從 object designer 分離工作流程 `generate-code` 和 `generate-glb-from-code` 為獨立檔案。另外，成立 schemas 管理共同 schemas 和 types。
    - 更新 object designer HTTP API Endpoints，以及它的客戶端 Python 腳本。

--- backend_server/services/instructions.ts
+++ backend_server/services/instructions.ts
@@ -1,9 +1,16 @@
 import path from "path";
+import { readFileSync } from "fs";
 import { readFile } from "fs/promises";
 import Handlebars from "handlebars";
 
 export async function loadInstructionsTemplate<T = {}>(name: string) {
   const filePath = path.resolve(process.cwd(), "instructions", `${name}.md`);
   const md = await readFile(filePath, "utf8");
+  return Handlebars.compile<T>(md);
+}
+
+export function loadInstructionsTemplateSync<T = {}>(name: string) {
+  const filePath = path.resolve(process.cwd(), "instructions", `${name}.md`);
+  const md = readFileSync(filePath, "utf8");
   return Handlebars.compile<T>(md);
 }
